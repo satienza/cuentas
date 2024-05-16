3afd15e35b05e213409705afd1aad2fdff097e63
Santiago Atienza Ferro <satienza@gmail.com>
2024-05-16 17:04:27 +0100

    Inicial

---
+++ src/cuentas/Desposito.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cuentas;
+
+/**
+ *
+ * @author satienza
+ */
+public class Desposito {
+
+    /**
+     * @param args the command line arguments
+     */
+public static void main(String[] args) {
+    CCuenta miCuenta;
+    double saldoActual;
+    miCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+    saldoActual = miCuenta.estado();
+    System.out.println("El saldo actual es "+ saldoActual );
+        operaciones(miCuenta);
+    saldoActual = miCuenta.estado();
+    System.out.println("El saldo actual es "+ saldoActual );
+}
+
+    public static void operaciones(CCuenta miCuenta) {
+        // Operaciones de ingreso y retiro
+        miCuenta.retirar(2300);
+        miCuenta.ingresar(685);
+    }
+    
+}
